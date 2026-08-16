cd040084e0e005735a790073b4cfc4e20b5d74ea
Jess Bees <hi@toomanybees.com>
2026-08-16 19:45:33 -0400

    parse robots json without caring about object values

--- src/classifier/robots_json.rs
+++ src/classifier/robots_json.rs
@@ -1,21 +1,25 @@
 use aho_corasick::{AhoCorasick, BuildError, Input, Match};
-use serde::Deserialize;
-use std::collections::HashMap;
-use std::fs::File;
-use std::path::Path;
+use serde::de::{Deserialize, Deserializer, IgnoredAny, MapAccess, Visitor};
+use std::{fmt, fs::File, path::Path};
 
-#[derive(Debug, Deserialize)]
-pub struct RobotsJsonEntry {
-    operator: String,
-    respect: String,
-    function: String,
-    frequency: String,
-    description: String,
+pub struct RobotsJson {
+    patterns: Vec<String>,
+    matcher: AhoCorasick,
 }
 
-#[derive(Debug)]
-pub struct RobotsJson {
-    matcher: AhoCorasick,
+impl fmt::Debug for RobotsJson {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct("RobotsJson")
+            .field("patterns", &self.patterns)
+            .field(
+                "matcher",
+                &format_args!(
+                    "AhoCorasick {{ memory usage: {} }}",
+                    self.matcher.memory_usage()
+                ),
+            )
+            .finish()
+    }
 }
 
 impl RobotsJson {
@@ -26,10 +30,12 @@ impl RobotsJson {
 
 pub fn load_robots_json<P: AsRef<Path>>(path: P) -> Result<RobotsJson, RobotsJsonError> {
     let f = File::open(path)?;
-    let json: HashMap<String, RobotsJsonEntry> = serde_json::from_reader(f)?;
-    let robots: Vec<_> = json.keys().cloned().collect();
-    let matcher = AhoCorasick::new(&robots)?;
-    Ok(RobotsJson { matcher })
+    let robots: UserAgents = serde_json::from_reader(f)?;
+    let matcher = AhoCorasick::new(&robots.0)?;
+    Ok(RobotsJson {
+        patterns: robots.0,
+        matcher,
+    })
 }
 
 #[derive(Debug)]
@@ -54,5 +60,57 @@ impl From<serde_json::Error> for RobotsJsonError {
 impl From<BuildError> for RobotsJsonError {
     fn from(e: BuildError) -> RobotsJsonError {
         RobotsJsonError::Matcher(e)
+    }
+}
+
+struct UserAgents(Vec<String>);
+
+struct RobotsVisitor;
+
+impl<'de> Visitor<'de> for RobotsVisitor {
+    type Value = UserAgents;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an arbitrary map at the root level")
+    }
+
+    fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        let mut vec = Vec::with_capacity(access.size_hint().unwrap_or(0));
+
+        while let Some((key, _)) = access.next_entry::<String, IgnoredAny>()? {
+            vec.push(key);
+        }
+
+        Ok(UserAgents(vec))
+    }
+}
+
+impl<'de> Deserialize<'de> for UserAgents {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_map(RobotsVisitor)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::UserAgents;
+
+    #[test]
+    fn serde_deserializes_object_keys() {
+        let json = r#"{
+            "key1": "literally anything",
+            "key2": 0,
+            "key3": { "literally": "anything" },
+            "key4": ["but", "watch", "those", "trailing", "commas"]
+        }"#;
+
+        let user_agents: UserAgents = serde_json::from_str(json).unwrap();
+        assert_eq!(user_agents.0, vec!["key1", "key2", "key3", "key4"]);
     }
 }
