640b5bad37cd2f6f04e5bc37a8a13561c0909d81
Jess Bees <hi@toomanybees.com>
2026-08-17 05:37:41 -0400

    parse more

--- src/config/parse.rs
+++ src/config/parse.rs
@@ -1,6 +1,7 @@
 use super::config::{Classifier, Config, Garbage, Links, Paragraphs, Server, ServerMode};
-use http::{StatusCode, status::InvalidStatusCode};
+use http::status::{InvalidStatusCode, StatusCode};
 use kdl::{KdlDocument, KdlError, KdlNode};
+use std::fmt::Debug;
 use std::fs;
 use std::net::AddrParseError;
 use std::num::TryFromIntError;
@@ -26,6 +27,12 @@ fn parse_doc(doc: KdlDocument) -> Result<Config, ParseError> {
             "server" => {
                 config.server = node.parse_server()?;
             }
+            "classifier" => {
+                config.classifier = node.parse_classifier()?;
+            }
+            "garbage" => {
+                config.garbage = node.parse_garbage()?;
+            }
             _ => {}
         }
     }
@@ -36,11 +43,17 @@ fn parse_doc(doc: KdlDocument) -> Result<Config, ParseError> {
 trait Parseable {
     fn parse_server(&self) -> Result<Server, ParseError>;
 
+    fn parse_classifier(&self) -> Result<Classifier, ParseError>;
+
+    fn parse_garbage(&self) -> Result<Garbage, ParseError>;
+
     fn one_string_arg(&self) -> Result<&str, ParseError>;
 
-    fn numeric_prop(&self, name: &str) -> Result<Option<i128>, ParseError>;
+    fn int_prop(&self, name: &str) -> Result<Option<i128>, ParseError>;
 
-    // fn string_seq(&self) -> Result<Vec<String>, ParseError>;
+    fn string_seq(&self) -> Result<Vec<String>, ParseError>;
+
+    fn int_seq<I: Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError>;
 }
 
 impl Parseable for KdlNode {
@@ -59,10 +72,10 @@ impl Parseable for KdlNode {
                     server.listen = child.one_string_arg()?.parse()?;
                 }
                 "status-codes" => {
-                    if let Some(status) = child.numeric_prop("valid")? {
+                    if let Some(status) = child.int_prop("valid")? {
                         server.status_code_valid = StatusCode::from_u16(u16::try_from(status)?)?;
                     }
-                    if let Some(status) = child.numeric_prop("spam")? {
+                    if let Some(status) = child.int_prop("spam")? {
                         server.status_code_spam = StatusCode::from_u16(u16::try_from(status)?)?;
                     }
                 }
@@ -71,6 +84,74 @@ impl Parseable for KdlNode {
         }
 
         Ok(server)
+    }
+
+    fn parse_classifier(&self) -> Result<Classifier, ParseError> {
+        let mut classifier = Classifier::default();
+
+        for child in self.iter_children() {
+            match child.name().value() {
+                "trusted-decision-header" => {
+                    classifier.trusted_decision_header = Some(child.one_string_arg()?.to_string());
+                }
+                "user-agents" => {
+                    for child in child.iter_children() {
+                        match child.name().value() {
+                            "robots-json-path" => {
+                                classifier.robots_json_path =
+                                    Some(child.one_string_arg()?.to_string())
+                            }
+                            "unwanted" => classifier.unwanted_agents = child.string_seq()?,
+                            "trusted" => classifier.trusted_agents = child.string_seq()?,
+                            _ => {}
+                        }
+                    }
+                }
+                "trusted-paths" => {
+                    classifier.trusted_paths = child.string_seq()?;
+                }
+                "asns" => {
+                    for child in child.iter_children() {
+                        match child.name().value() {
+                            "database" => {
+                                classifier.asns_db_path = Some(child.one_string_arg()?.to_string())
+                            }
+                            "unwanted" => classifier.unwanted_asns = child.int_seq()?,
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        Ok(classifier)
+    }
+
+    fn parse_garbage(&self) -> Result<Garbage, ParseError> {
+        let mut garbage = Garbage::default();
+
+        for child in self.iter_children() {
+            match child.name().value() {
+                "corpus-files" => {
+                    garbage.source_files = child.string_seq()?;
+                }
+                "words-file" => {
+                    garbage.words_file = Some(child.one_string_arg()?.to_string());
+                }
+                "template-file" => {
+                    garbage.template_file = Some(child.one_string_arg()?.to_string());
+                }
+                "poisons" => {
+                    // garbage.poisons = child.string_seq()?;
+                }
+                "paragraphs" => {}
+                "links" => {}
+                _ => {}
+            }
+        }
+
+        Ok(garbage)
     }
 
     fn one_string_arg(&self) -> Result<&str, ParseError> {
@@ -95,7 +176,7 @@ impl Parseable for KdlNode {
             })
     }
 
-    fn numeric_prop(&self, name: &str) -> Result<Option<i128>, ParseError> {
+    fn int_prop(&self, name: &str) -> Result<Option<i128>, ParseError> {
         self.get(name)
             .map(|prop| {
                 prop.as_integer().ok_or_else(|| ParseError::Invalid {
@@ -107,6 +188,62 @@ impl Parseable for KdlNode {
                 })
             })
             .transpose()
+    }
+
+    fn string_seq(&self) -> Result<Vec<String>, ParseError> {
+        let mut seq = Vec::new();
+        for entry in self.iter() {
+            if let Some(_name) = entry.name() {
+                return Err(ParseError::Invalid {
+                    field: self.name().value().to_string(),
+                    message: format!("{} must not have named properties", self.name().value()),
+                });
+            }
+            match entry.value().as_string() {
+                Some(s) => seq.push(s.to_string()),
+                None => {
+                    return Err(ParseError::Invalid {
+                        field: self.name().value().to_string(),
+                        message: format!("{} must be a sequence of strings", self.name().value()),
+                    });
+                }
+            }
+        }
+        seq.shrink_to_fit();
+        Ok(seq)
+    }
+
+    fn int_seq<I: Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError> {
+        let mut seq = Vec::new();
+        for entry in self.iter() {
+            if let Some(_name) = entry.name() {
+                return Err(ParseError::Invalid {
+                    field: self.name().value().to_string(),
+                    message: format!("{} must not have named properties", self.name().value()),
+                });
+            }
+            match entry.value().as_integer() {
+                Some(i) => {
+                    let converted = I::try_from(i).map_err(|_| ParseError::Invalid {
+                        field: self.name().value().to_string(),
+                        message: format!("it doesn't work, babe"),
+                    })?;
+                    seq.push(converted);
+                }
+                None => {
+                    return Err(ParseError::Invalid {
+                        field: self.name().value().to_string(),
+                        message: format!(
+                            "{} must be a sequence of {} integers",
+                            self.name().value(),
+                            std::any::type_name::<I>()
+                        ),
+                    });
+                }
+            }
+        }
+        seq.shrink_to_fit();
+        Ok(seq)
     }
 }
 
