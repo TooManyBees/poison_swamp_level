360dfaee104a04cdf6786b3784865b9492936d16
Jess Bees <hi@toomanybees.com>
2026-08-17 21:02:01 -0400

    make node wrangling just a little more pleasant

--- src/config/parse.rs
+++ src/config/parse.rs
@@ -1,7 +1,8 @@
 use super::config::{Classifier, Config, Garbage, Server, ServerMode};
 use http::status::{InvalidStatusCode, StatusCode};
 use kdl::{KdlDocument, KdlEntry, KdlError, KdlNode};
-use std::{fmt::Debug, fs, net::AddrParseError, num::TryFromIntError, path::Path};
+use std::ops::Deref;
+use std::{fmt, fs, net::AddrParseError, num::TryFromIntError, path::Path};
 
 pub fn load_config<P: AsRef<Path>>(path: P) -> Result<Config, ParseError> {
     let doc = load_file(path)?;
@@ -11,7 +12,7 @@ pub fn load_config<P: AsRef<Path>>(path: P) -> Result<Config, ParseError> {
 
 fn load_file<P: AsRef<Path>>(path: P) -> Result<KdlDocument, ParseError> {
     let s = fs::read_to_string(path)?;
-    let doc = s.parse::<KdlDocument>().unwrap();
+    let doc = s.parse::<KdlDocument>()?;
     Ok(doc)
 }
 
@@ -43,36 +44,75 @@ trait Parseable {
 
     fn parse_garbage(&self) -> Result<Garbage, ParseError>;
 
-    fn one_string_arg(&self) -> Result<&str, ParseError>;
+    fn one_string_entry<'a>(&'a self) -> Result<StringEntry<'a>, ParseError>;
 
-    fn int_prop<I: Debug + TryFrom<i128>>(&self, name: &str) -> Result<Option<I>, ParseError>;
+    fn one_string_arg(&self) -> Result<String, ParseError>;
+
+    fn int_prop<I: fmt::Debug + TryFrom<i128>>(&self, name: &str) -> Result<Option<I>, ParseError> {
+        let res = self.int_prop_with_entry(name)?;
+        Ok(res.map(|(_, i)| i))
+    }
+
+    fn int_prop_with_entry<I: fmt::Debug + TryFrom<i128>>(
+        &self,
+        name: &str,
+    ) -> Result<Option<(&KdlEntry, I)>, ParseError>;
 
     fn string_seq(&self) -> Result<Vec<String>, ParseError>;
 
-    fn int_seq<I: Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError>;
+    fn int_seq<I: fmt::Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError>;
+}
+
+struct StringEntry<'a>(&'a KdlEntry, &'a str);
+impl<'a> Deref for StringEntry<'a> {
+    type Target = KdlEntry;
+    fn deref(&self) -> &Self::Target {
+        self.0
+    }
+}
+impl<'a> AsRef<str> for StringEntry<'a> {
+    fn as_ref(&self) -> &str {
+        self.1
+    }
 }
 
 impl Parseable for KdlNode {
     fn parse_server(&self) -> Result<Server, ParseError> {
         let mut server = Server::default();
 
-        match self.one_string_arg()? {
+        let mode_entry = self.one_string_entry()?;
+        match mode_entry.as_ref() {
             "proxy" => server.mode = ServerMode::Proxy,
             "preflight" => server.mode = ServerMode::Preflight,
-            m => panic!("never heard of a {m} mode"),
+            _ => {
+                return Err(ParseError::from_entry(
+                    &mode_entry,
+                    "unsupported server mode".into(),
+                ));
+            }
         }
 
         for child in self.iter_children() {
             match child.name().value() {
                 "listen" => {
-                    server.listen = child.one_string_arg()?.parse()?;
+                    let entry = child.one_string_entry()?;
+                    server.listen = entry.as_ref().parse().map_err(|_| {
+                        ParseError::from_entry(&entry, "invalid socket address".into())
+                    })?;
                 }
                 "status-codes" => {
-                    if let Some(status) = child.int_prop::<u16>("valid")? {
-                        server.status_code_valid = StatusCode::from_u16(status)?;
+                    if let Some((entry, status)) = child.int_prop_with_entry::<u16>("valid")? {
+                        server.status_code_valid = StatusCode::from_u16(status).map_err(|_| {
+                            ParseError::from_entry(entry, "invalid HTTP status code".into())
+                        })?;
                     }
-                    if let Some(status) = child.int_prop::<u16>("spam")? {
-                        server.status_code_spam = StatusCode::from_u16(status)?;
+                    if let Some((entry, status)) = child.int_prop_with_entry::<u16>("spam")? {
+                        server.status_code_spam = StatusCode::from_u16(status).map_err(|_| {
+                            ParseError::from_entry(
+                                entry,
+                                "invalid HTTP status code".into(),
+                            )
+                        })?;
                     }
                 }
                 _ => {}
@@ -88,14 +128,13 @@ impl Parseable for KdlNode {
         for child in self.iter_children() {
             match child.name().value() {
                 "trusted-decision-header" => {
-                    classifier.trusted_decision_header = Some(child.one_string_arg()?.to_string());
+                    classifier.trusted_decision_header = Some(child.one_string_arg()?);
                 }
                 "user-agents" => {
                     for child in child.iter_children() {
                         match child.name().value() {
                             "robots-json-path" => {
-                                classifier.robots_json_path =
-                                    Some(child.one_string_arg()?.to_string())
+                                classifier.robots_json_path = Some(child.one_string_arg()?)
                             }
                             "unwanted" => classifier.unwanted_agents = child.string_seq()?,
                             "trusted" => classifier.trusted_agents = child.string_seq()?,
@@ -109,9 +148,7 @@ impl Parseable for KdlNode {
                 "asns" => {
                     for child in child.iter_children() {
                         match child.name().value() {
-                            "database" => {
-                                classifier.asns_db_path = Some(child.one_string_arg()?.to_string())
-                            }
+                            "database" => classifier.asns_db_path = Some(child.one_string_arg()?),
                             "unwanted" => classifier.unwanted_asns = child.int_seq()?,
                             _ => {}
                         }
@@ -133,10 +170,10 @@ impl Parseable for KdlNode {
                     garbage.source_files = child.string_seq()?;
                 }
                 "words-file" => {
-                    garbage.words_file = Some(child.one_string_arg()?.to_string());
+                    garbage.words_file = Some(child.one_string_arg()?);
                 }
                 "template-file" => {
-                    garbage.template_file = Some(child.one_string_arg()?.to_string());
+                    garbage.template_file = Some(child.one_string_arg()?);
                 }
                 "poisons" => {
                     garbage.poisons = child.string_seq()?;
@@ -194,18 +231,27 @@ impl Parseable for KdlNode {
         Ok(garbage)
     }
 
-    fn one_string_arg(&self) -> Result<&str, ParseError> {
-        let entry = self
-            .entry(0)
-            .ok_or_else(|| ParseError::from_node(self, "must have one string argument".into()))?;
-        match entry.value().as_string() {
-            Some("") => Err(ParseError::from_entry(entry, "must not be empty".into())),
-            Some(arg) => Ok(arg),
-            None => Err(ParseError::from_entry(entry, "must be a string".into())),
+    fn one_string_entry<'a>(&'a self) -> Result<StringEntry<'a>, ParseError> {
+        self.entry(0)
+            .and_then(|e| match e.value().as_string() {
+                Some(s) => Some(StringEntry(e, s)),
+                None => None,
+            })
+            .ok_or_else(|| ParseError::from_node(self, "must have one string argument".into()))
+    }
+
+    fn one_string_arg(&self) -> Result<String, ParseError> {
+        let entry = self.one_string_entry()?;
+        match entry.as_ref() {
+            "" => Err(ParseError::from_entry(&entry, "must not be empty".into())),
+            arg => Ok(arg.to_string()),
         }
     }
 
-    fn int_prop<I: Debug + TryFrom<i128>>(&self, name: &str) -> Result<Option<I>, ParseError> {
+    fn int_prop_with_entry<I: fmt::Debug + TryFrom<i128>>(
+        &self,
+        name: &str,
+    ) -> Result<Option<(&KdlEntry, I)>, ParseError> {
         self.iter()
             .find(|e| e.name().map(|n| n.value() == name).unwrap_or(false))
             .map(|entry| {
@@ -213,6 +259,7 @@ impl Parseable for KdlNode {
                     .value()
                     .as_integer()
                     .and_then(|n| n.try_into().ok())
+                    .map(|n| (entry, n))
                     .ok_or_else(|| {
                         ParseError::from_entry(
                             entry,
@@ -243,7 +290,7 @@ impl Parseable for KdlNode {
         Ok(seq)
     }
 
-    fn int_seq<I: Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError> {
+    fn int_seq<I: fmt::Debug + TryFrom<i128>>(&self) -> Result<Vec<I>, ParseError> {
         let mut seq = Vec::new();
         for entry in self.iter() {
             if entry.name().is_some() {
