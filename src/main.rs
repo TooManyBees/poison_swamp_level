001dd0e14de53e9cf112a5d2d3323cd2369ae193
Jess Bees <hi@toomanybees.com>
2026-08-22 02:16:01 -0400

    encapsulate creation of app for each request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use hyper::server::conn::http1;
 use hyper_util::rt::TokioIo;
-use poison_swamp_level::handler::{App, preflight, proxy};
+use poison_swamp_level::handler::{App, HandlerType, preflight, proxy};
 use poison_swamp_level::{Classifier, Config, Garbage, ServerMode};
-use std::sync::Arc;
+use std::{
+    error::Error,
+    net::{IpAddr, SocketAddr},
+    sync::Arc,
+};
 use tokio::net::TcpListener;
 
 #[tokio::main(flavor = "local")]
@@ -19,37 +23,72 @@ async fn main() {
         .filter(None, config.logging.level)
         .init();
 
-    let classifier = Classifier::new(&config).unwrap();
-    let garbage = Garbage::new(&config).unwrap();
+    // TODO: signal handler for reloading config (except log level)
+    // on signal, background thread to load the config and create the
+    // generator/classifier
+    // tokio::select! to loop between listener, signal handler, config reload result
+    // if config.server.listen changes, break out of entire loop and restart it
 
-    let classifier = Arc::new(classifier);
-    let garbage = Arc::new(garbage);
+    let app_config = AppConfig::from_config(config).unwrap();
 
-    let listener = TcpListener::bind(config.server.listen).await.unwrap();
-
-    let handler = match config.server.mode {
-        ServerMode::Proxy => proxy,
-        ServerMode::Preflight => preflight,
-    };
-
-    log::info!("Listening on {}", config.server.listen);
+    let listener = app_config.listen().await.unwrap();
 
     loop {
         let (stream, addr) = listener.accept().await.unwrap();
         let io = TokioIo::new(stream);
-        let app = App {
-            client_ip: addr.ip(),
-            classifier: classifier.clone(),
-            garbage: garbage.clone(),
-            handler,
-            status_code_valid: config.server.status_code_valid,
-            status_code_spam: config.server.status_code_spam,
-            logging: config.logging.request_handler,
-        };
+        let app = app_config.to_service(addr.ip());
         tokio::task::spawn(async move {
             if let Err(e) = http1::Builder::new().serve_connection(io, app).await {
                 println!("Failed to serve connection: {e}");
             }
         });
+    }
+}
+
+struct AppConfig {
+    config: Config,
+    classifier: Arc<Classifier>,
+    garbage: Arc<Garbage>,
+    handler: HandlerType,
+}
+
+impl AppConfig {
+    fn from_config(config: Config) -> Result<Self, Box<dyn Error>> {
+        let classifier = Classifier::new(&config).unwrap(); // FIXME
+        let garbage = Garbage::new(&config).unwrap(); // FIXME
+
+        let handler = match config.server.mode {
+            ServerMode::Proxy => proxy,
+            ServerMode::Preflight => preflight,
+        };
+
+        Ok(AppConfig {
+            config,
+            classifier: Arc::new(classifier),
+            garbage: Arc::new(garbage),
+            handler,
+        })
+    }
+
+    fn to_service(&self, client_ip: IpAddr) -> App {
+        App {
+            client_ip,
+            classifier: self.classifier.clone(),
+            garbage: self.garbage.clone(),
+            handler: self.handler,
+            status_code_valid: self.config.server.status_code_valid,
+            status_code_spam: self.config.server.status_code_spam,
+            logging: self.config.logging.request_handler,
+        }
+    }
+
+    fn listen_addr(&self) -> SocketAddr {
+        self.config.server.listen
+    }
+
+    async fn listen(&self) -> std::io::Result<TcpListener> {
+        let listener = TcpListener::bind(self.listen_addr()).await?;
+        log::info!("Listening on {}", self.listen_addr());
+        Ok(listener)
     }
 }
