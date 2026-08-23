70d4f5da20c7f29cbb7543af0a2d98fc1eab8e07
Jess Bees <hi@toomanybees.com>
2026-08-23 13:48:16 -0400

    benchmark both generator variants

---
+++ benches/markov_generator.rs
@@ -0,0 +1,32 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use poison_swamp_level::Corpus;
+use rand_seeder::{Seeder, SipRng};
+use std::hint::black_box;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let corpus = Corpus::from_files(&["susan.sontag.on.style.txt"]).unwrap();
+    c.bench_function("map-based generator", |b| {
+        let mut rng: SipRng = Seeder::from("/some/predictable/path").into_rng();
+        b.iter(|| {
+            let generator = corpus.generator(black_box(&mut rng));
+            let words = generator.take(500);
+            for word in words {
+                black_box(word);
+            }
+        })
+    });
+
+    c.bench_function("list-based generator", |b| {
+        let mut rng: SipRng = Seeder::from("/some/predictable/path").into_rng();
+        b.iter(|| {
+            let generator = corpus.generator2(black_box(&mut rng));
+            let words = generator.take(500);
+            for word in words {
+                black_box(word);
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
